f8e7cdb894df782f3b67d14f450754d79a322e9f
Dylan Cree <dylanpab1@gmail.com>
2022-03-17 12:25:38 +0000

    Added while and switch statement to control the program via options presented to the user

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,10 @@ package com.company;
 
 import com.opencsv.exceptions.CsvValidationException;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
+    private final Scanner scanner = new Scanner(System.in);
     private final Loans loan = new Loans();
     private final Users user = new Users();
     private final Items item = new Items();
@@ -18,10 +20,50 @@ public class Main {
     }
 
     private void start(){
-        boolean flag = true;
-            while (flag){
-                System.out.println("Test print");
-                loanReader.loanPrint();
+        boolean quit = false;
+        int choice;
+        printInstructions();
+
+        while (!quit){
+                System.out.println("Enter your choice: ");
+                choice = scanner.nextInt();
+                scanner.nextLine();
+
+            switch (choice){
+                case 0:
+                    printInstructions();
+                    break;
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+                    quit = true;
+                    System.out.println("Program has terminated");
+                    break;
             }
+            }
+    }
+
+    private static void printInstructions(){
+        System.out.println("\nPress ");
+        System.out.println("\t 0 - To print choice options.");
+        System.out.println("\t 1 - To print the list of Items.");
+        System.out.println("\t 2 - To add an item to the list.");
+        System.out.println("\t 3 - To modify an item in the list.");
+        System.out.println("\t 4 - To remove an item from the list.");
+        System.out.println("\t 5 - To search for an item in the list.");
+        System.out.println("\t 6 - TO quit the application.");
     }
 }
