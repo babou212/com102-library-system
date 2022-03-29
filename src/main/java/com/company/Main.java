f38603472b6515a0976b94d12979f2f6e0c440a5
Dylan Cree <dylanpab1@gmail.com>
2022-03-29 23:59:01 +0100

    small refactor

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -61,6 +61,7 @@ public class Main {
                     break;
                 case 5:
                     loanService.printLoans();
+                    System.out.print("\n");
                     loanService.printItems();
                     break;
                 case 6:
